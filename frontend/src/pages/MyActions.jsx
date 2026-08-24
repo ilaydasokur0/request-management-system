d89c5a39cc80f256d5e24c61a89f8d675ee65f12
Ilayda Sokur <ilaydasokur0@gmail.com>
2026-08-24 16:35:58 +0300

    add past and current action pages

--- frontend/src/pages/MyActions.jsx
+++ frontend/src/pages/MyActions.jsx
@@ -1,13 +1,16 @@
 import '../App.css'
+import {useSearchParams} from 'react-router-dom'
 
 function MyActions() {
+const [searchParams] = useSearchParams(); // URL'deki sorgu parametrelerini alır
+const view = searchParams.get('view'); // view parametresi var mı varsa değerini alır
     return (
         <section className="my-actions-page">
             <div>
                 <section className="header">
-                    <h1>İşlemdeki Talepler</h1>
+                    <h1>{view === 'past' ? 'Geçmiş İşlemler' : 'Aktif İşlemler'}</h1>
                     <div className="header-line"></div>
-                    <p>İşleme aldığınız talepleri görüntüleyebilir ve yönetebilirsiniz.</p>
+                    <p>{view === 'past' ? 'Geçmiş işlemleriniz burada görüntüleyebilirsiniz' : 'Aktif işlemlerinizi burada görüntüleyebilir ve yönetebilirsiniz.'}</p>
                 </section>
         <section className="table-section"> 
             <table className="actions-table">
@@ -16,10 +19,9 @@ function MyActions() {
                         <th>Talep ID</th>
                         <th>Talep Başlığı</th>
                         <th>Talep Açıklaması</th>
-                        <th>Talep Durumu</th>
                         <th>Öncelik</th>
-                        <th>Oluşturulma Tarihi</th>
-                        <th>İşleme Alınma Tarihi</th>
+                        <th>Talep Oluşturulma Tarihi</th>
+                        <th>{view === 'past' ? 'İşlem Tamamlanma Tarihi' : 'İşleme Alınma Tarihi'}</th>
                     </tr>
                 </thead>
                 <tbody className="actions-list" id="actions-list">
