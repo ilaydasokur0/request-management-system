187de2284fd6e1d6f35b592b2e8e6adf77d44878
Ilayda Sokur <ilaydasokur0@gmail.com>
2026-08-25 15:55:55 +0300

    add requestdetail.jsx

--- frontend/src/pages/RequestDetail.jsx
+++ frontend/src/pages/RequestDetail.jsx
@@ -1,16 +1,34 @@
 import '../App.css';
+import { useParams } from 'react-router-dom';
+import { mockRequests } from '../data/mockRequests';
 
 function RequestDetail() {
+    const { id } = useParams();
+    const request = mockRequests.find((request) => request.id === parseInt(id));
     return (
-        <section className="request-page">
+        <section className="request-detail-page">
             <div>
                 <section className="header">
-                    <h1>Talep Detayları</h1>
+                    <h1>Talep Detayı</h1>
                     <div className="header-line"></div>
-                    <p>Seçtiğiniz talebin detaylarını görüntüleyebilir ve yönetebilirsiniz.</p>
+                    <p>Talep detaylarını burada görüntüleyebilirsiniz.</p>
+                </section>
+                <section className="request-detail-section">
+                    <dl className="request-detail">
+                        <dt>Talep Başlığı:</dt>
+                        <dd>{request.title}</dd>
+                        <dt>Talep Açıklaması:</dt>
+                        <dd>{request.description}</dd>
+                        <dt>Talep Durumu:</dt>
+                        <dd>{request.status}</dd>
+                        <dt>Öncelik:</dt>
+                        <dd>{request.priority}</dd>
+                        <dt>Oluşturulma Tarihi:</dt>
+                        <dd>{request.createdAt}</dd>
+                    </dl>
                 </section>
             </div>
         </section>
-    )
+    );
 }
 export default RequestDetail
