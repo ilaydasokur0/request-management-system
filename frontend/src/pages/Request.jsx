f2e4c7b5bba67f9105828e549e31ddb6b8dd6b0e
Ilayda Sokur <ilaydasokur0@gmail.com>
2026-08-25 14:06:02 +0300

    add request.jsx

---
+++ frontend/src/pages/Request.jsx
@@ -0,0 +1,17 @@
+import '../App.css;'
+
+function Request() {
+    return (
+        <section className="request-page">
+            <div>
+                <section className="header">
+                    <h1>Talep Detayları</h1>
+                    <div className="header-line"></div>
+                    <p>Seçtiğiniz talebin detaylarını görüntüleyebilir ve yönetebilirsiniz.</p>
+                </section>
+            </div>
+        </section>
+    )
+}
+
+export default Request
