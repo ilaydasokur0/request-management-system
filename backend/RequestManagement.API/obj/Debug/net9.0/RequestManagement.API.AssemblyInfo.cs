ccd851c054671dc5ea792bc709e31fbf8def1ecc
Ilayda Sokur <ilaydasokur0@gmail.com>
2026-08-26 11:26:17 +0300

    update mock data

--- backend/RequestManagement.API/obj/Debug/net9.0/RequestManagement.API.AssemblyInfo.cs
+++ backend/RequestManagement.API/obj/Debug/net9.0/RequestManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RequestManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cee90be9d6c06731ec3e77ef7a5c7b4ef542487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9190df8d300325a10c44afef2757a10e4593bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RequestManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RequestManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
